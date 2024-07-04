474e635e297459164852f6f4ea5950b43f93e85e
JustTrott <greenvisp@gmail.com>
2024-07-04 13:16:03 +0500

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "APhB",
+	title: "NChB",
 	description: "Created by Tima and Nuri",
 };
 
